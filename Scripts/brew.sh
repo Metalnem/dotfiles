e99cc15c18218eb6834cb62d8ba5e602904d20f6
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-04-25 21:14:26 +0200

    Update brew script to install latest bash

--- Scripts/brew.sh
+++ Scripts/brew.sh
@@ -6,11 +6,18 @@ brew upgrade
 brew tap caskroom/cask
 brew cask install kdiff3
 
+brew install bash
 brew install ffmpeg
 brew install grep
 brew install node
 brew install rsync
 brew install tree
 brew install unrar
+
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+	echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+	chsh -s /usr/local/bin/bash;
+fi;
 
 brew cleanup
