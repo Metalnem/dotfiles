1ef85f904786dc122df33a22ce27bc6dc33a5aa3
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-06-03 21:11:45 +0200

    Update backup script

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -2,6 +2,6 @@
 
 cd ~/Data
 
-rsync --delete-after --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -avzL --iconv=utf-8-mac,utf-8 --exclude='.DS_Store' \
+rsync --delete-before -avzL --iconv=utf-8-mac,utf-8 --exclude='.DS_Store' \
 	~/Dropbox/Apps ./Audible ~/Dropbox/Important ~/Dropbox/Literature ./Music ./Pictures ~/Dropbox/Projects ./Software ./Videos \
 	/Volumes/Metalnem
