5cec16be9a2b31784139147bbc7293864a0a684b
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-06-04 12:34:25 +0200

    Remove backup script

--- Scripts/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cd ~/Data
-
-rsync --delete-before -avzL --iconv=utf-8-mac,utf-8 --exclude='.DS_Store' \
-	~/Dropbox/Apps ./Audible ~/Dropbox/Important ~/Dropbox/Literature ./Music ./Pictures ~/Dropbox/Projects ./Software ./Videos \
-	/Volumes/Metalnem
