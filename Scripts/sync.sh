522821afca72791658c20ac493e872cc2d29e43f
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-06-25 21:39:23 +0200

    Remove Audible from backup

--- Scripts/sync.sh
+++ Scripts/sync.sh
@@ -3,7 +3,7 @@
 cd ~/Data
 
 rsync --delete-after --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -avzL --iconv=utf-8-mac,utf-8 --exclude='.DS_Store' \
-	~/Dropbox/Apps ./Audible ~/Dropbox/Important ~/Dropbox/Literature ./Music ./Pictures ~/Dropbox/Projects ./Software ./Videos \
+	~/Dropbox/Apps ~/Dropbox/Important ~/Dropbox/Literature ./Music ./Pictures ~/Dropbox/Projects ./Software ./Videos \
 	nemanja@prase:/mnt/raid/backup/
 
 ssh prase "ls /mnt/raid/public/ -LR" > ~/Dropbox/Important/Storage.txt
