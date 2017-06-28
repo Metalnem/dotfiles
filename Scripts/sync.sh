878b2c592ee299226a516f5dc42be2f42325e917
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-06-28 16:26:00 +0200

    Update sync script

--- Scripts/sync.sh
+++ Scripts/sync.sh
@@ -3,10 +3,7 @@
 cd ~/Data
 
 rsync --delete-after --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -avzL --iconv=utf-8-mac,utf-8 --exclude='.DS_Store' \
-	~/Dropbox/Apps ~/Dropbox/Important ~/Dropbox/Literature ./Music ./Pictures ~/Dropbox/Projects ./Software ./Videos \
+	~/Dropbox/Apps ~/Dropbox/Books ~/Dropbox/Important ./Music ./Pictures ~/Dropbox/Projects ./Software ./Videos \
 	nemanja@prase:/mnt/raid/backup/
 
 ssh prase "ls /mnt/raid/public/ -LR" > ~/Dropbox/Important/Storage.txt
-
-rsync --delete-after --chmod=Du=rwx,Dgo=rx,Fu=rw,Fgo=r -avz --iconv=utf-8-mac,utf-8 --exclude='.DS_Store' \
-	~/Private nemanja@prase:/mnt/raid/public/
