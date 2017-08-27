6ffeb3bb8d3d3fb37e6a045079ab59e6f560804e
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-08-27 12:20:46 +0200

    Update backup script

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-rsync --delete -avz --exclude='.DS_Store' \
+rsync --delete -avzL --exclude='.DS_Store' \
 	~/Dropbox/Apps \
 	~/Dropbox/Books \
 	~/Dropbox/Important \
+	~/Dropbox/Music/Amazon \
+	~/Dropbox/Music/iTunes \
+	~/Dropbox/Music/Other \
 	~/Dropbox/Music/Rips \
 	~/Dropbox/Pictures \
 	~/Dropbox/Projects \
