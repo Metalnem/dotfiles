f3c40617125f0b894235cf34beae8f05fbed0602
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-08-29 15:51:39 +0200

    Add checksum script

---
+++ Scripts/checksum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/Dropbox/Important
+go get github.com/metalnem/hashes
+
+$GOPATH/bin/hashes create \
+	~/Dropbox/Books \
+	~/Dropbox/Music/Amazon \
+	~/Dropbox/Music/Bandcamp \
+	~/Dropbox/Music/Other \
+	~/Dropbox/Music/Rips \
+	~/Dropbox/Pictures \
+	~/Dropbox/Software \
+	~/Dropbox/Videos \
+	~/Music/iTunes/iTunes\ Media/Music
