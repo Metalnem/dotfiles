6a2d9d3fa8736f815e74b3309954864cff83aac9
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-01-04 17:50:40 +0100

    Update the checksum script

--- Scripts/checksum.sh
+++ Scripts/checksum.sh
@@ -6,13 +6,8 @@ go get github.com/metalnem/hashes
 $GOPATH/bin/hashes create \
 	~/Dropbox/Books \
 	~/Dropbox/Crypto \
-	~/Dropbox/Music/Amazon \
-	~/Dropbox/Music/Bandcamp \
-	~/Dropbox/Music/NoBusiness\ Records \
-	~/Dropbox/Music/Other \
-	~/Dropbox/Music/Rips \
+	~/Dropbox/Music \
 	~/Dropbox/Pictures \
 	~/Dropbox/Private \
 	~/Dropbox/Software \
-	~/Dropbox/Videos \
-	~/Music/iTunes/iTunes\ Media/Music
+	~/Dropbox/Videos
