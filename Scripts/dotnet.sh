f9ac873797236d93ffa80ee6db1e76cb33e9f5cc
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-05-17 16:49:52 +0200

    Add .NET Core install script for Raspberry Pi

---
+++ Scripts/dotnet.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+apt-get -y update
+sudo apt-get -y install libunwind8 gettext
+wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/2.1.300-rc1-008673/dotnet-sdk-2.1.300-rc1-008673-linux-arm.tar.gz
+sudo mkdir /opt/dotnet
+sudo tar -xvf dotnet-sdk-latest-linux-arm.tar.gz -C /opt/dotnet
+sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+dotnet --info
