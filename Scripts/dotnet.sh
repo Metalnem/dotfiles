7fb4bbed7a320a0032e2f517179b986db068fa6b
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-05-22 15:38:55 +0200

    Update dotnet script to download runtime instead of SDK

--- Scripts/dotnet.sh
+++ Scripts/dotnet.sh
@@ -2,8 +2,8 @@
 
 apt-get -y update
 sudo apt-get -y install libunwind8 gettext
-wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/2.1.300-rc1-008673/dotnet-sdk-2.1.300-rc1-008673-linux-arm.tar.gz
+wget https://dotnetcli.blob.core.windows.net/dotnet/Runtime/release/2.1/dotnet-runtime-latest-linux-arm64.tar.gz
 sudo mkdir /opt/dotnet
-sudo tar -xvf dotnet-sdk-2.1.300-rc1-008673-linux-arm.tar.gz -C /opt/dotnet
+sudo tar -xvf dotnet-runtime-latest-linux-arm64.tar.gz -C /opt/dotnet
 sudo ln -s /opt/dotnet/dotnet /usr/local/bin
 dotnet --info
