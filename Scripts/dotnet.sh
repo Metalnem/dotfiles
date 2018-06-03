2a760099c396b863622d433768e12a0dde87f83a
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-06-03 09:20:11 +0200

    Update dotnet.sh

--- Scripts/dotnet.sh
+++ Scripts/dotnet.sh
@@ -1,9 +1,9 @@
 #/bin/bash
 
 apt-get -y update
-sudo apt-get -y install libunwind8 gettext
-wget https://dotnetcli.blob.core.windows.net/dotnet/Runtime/release/2.1/dotnet-runtime-latest-linux-arm64.tar.gz
-sudo mkdir /opt/dotnet
-sudo tar -xvf dotnet-runtime-latest-linux-arm64.tar.gz -C /opt/dotnet
-sudo ln -s /opt/dotnet/dotnet /usr/local/bin
+apt-get -y install libunwind8 gettext
+wget https://dotnetcli.blob.core.windows.net/dotnet/Sdk/2.1.300/dotnet-sdk-2.1.300-linux-arm64.tar.gz
+mkdir /opt/dotnet
+tar -xvf dotnet-sdk-2.1.300-linux-arm64.tar.gz -C /opt/dotnet
+ln -s /opt/dotnet/dotnet /usr/local/bin
 dotnet --info
