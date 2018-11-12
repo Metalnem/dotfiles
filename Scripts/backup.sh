8ecc0cda5e3d9ba36509015368b7accc692af52d
Nemanja Mijailovic <metalnem@mijailovic.net>
2018-11-12 15:25:27 +0100

    Update sync scripts

--- Scripts/backup.sh
+++ Scripts/backup.sh
@@ -13,6 +13,7 @@ rsync --delete -avzL --exclude='.DS_Store' \
 	~/OneDrive/Music/Other \
 	~/OneDrive/Music/Rips \
 	~/OneDrive/Other \
+	~/OneDrive/Papers \
 	~/OneDrive/Pictures \
 	~/OneDrive/Private \
 	~/OneDrive/Projects \
