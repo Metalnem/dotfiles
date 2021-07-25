de22372acc1772ad6fded71a612426b4e68187bc
Nemanja Mijailovic <metalnem@mijailovic.net>
2021-07-25 10:49:43 +0200

    Update backup scripts

--- Scripts/backup1.sh
+++ Scripts/backup1.sh
@@ -5,10 +5,29 @@ rsync --delete-after -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
 	~/OneDrive/Books \
 	~/OneDrive/Documents \
 	~/OneDrive/Important \
-	~/OneDrive/Music \
 	~/OneDrive/Other \
 	~/OneDrive/Pictures \
 	~/OneDrive/Projects \
 	~/OneDrive/Software \
 	~/OneDrive/Videos \
 	/Volumes/Backup1
+
+rsync --delete-after -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
+	~/OneDrive/Music/Amazon \
+	~/OneDrive/Music/Cafe\ OTO \
+	~/OneDrive/Music/iTunes \
+	~/OneDrive/Music/NoBusiness\ Records \
+	~/OneDrive/Music/Other \
+	~/OneDrive/Music/Rips \
+	~/OneDrive/Music/Torrents \
+	~/OneDrive/Music/Vinyl \
+	~/OneDrive/Music/YouTube \
+	/Volumes/Backup1/Music
+
+rsync -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
+	~/OneDrive/Archive\ \(latest\)/ \
+	/Volumes/Backup1/Archive
+
+rsync -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
+	~/OneDrive/Bandcamp\ \(latest\)/ \
+	/Volumes/Backup1/Music/Bandcamp
--- Scripts/backup2.sh
+++ Scripts/backup2.sh
@@ -5,10 +5,29 @@ rsync --delete-after -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
 	~/OneDrive/Books \
 	~/OneDrive/Documents \
 	~/OneDrive/Important \
-	~/OneDrive/Music \
 	~/OneDrive/Other \
 	~/OneDrive/Pictures \
 	~/OneDrive/Projects \
 	~/OneDrive/Software \
 	~/OneDrive/Videos \
 	/Volumes/Backup2
+
+rsync --delete-after -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
+	~/OneDrive/Music/Amazon \
+	~/OneDrive/Music/Cafe\ OTO \
+	~/OneDrive/Music/iTunes \
+	~/OneDrive/Music/NoBusiness\ Records \
+	~/OneDrive/Music/Other \
+	~/OneDrive/Music/Rips \
+	~/OneDrive/Music/Torrents \
+	~/OneDrive/Music/Vinyl \
+	~/OneDrive/Music/YouTube \
+	/Volumes/Backup2/Music
+
+rsync -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
+	~/OneDrive/Archive\ \(latest\)/ \
+	/Volumes/Backup2/Archive
+
+rsync -avzL --iconv=utf-8,utf-8-mac --exclude='.DS_Store' \
+	~/OneDrive/Bandcamp\ \(latest\)/ \
+	/Volumes/Backup2/Music/Bandcamp
